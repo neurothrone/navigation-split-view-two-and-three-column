368fb53736447ff0f7d9161336812977908b0ab6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-13 01:22:04 +0100

    Initial Commit

---
+++ NavigationSplitViewTwoAndThreeColumn/NavigationSplitViewTwoAndThreeColumnApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewTwoAndThreeColumnApp.swift
+//  NavigationSplitViewTwoAndThreeColumn
+//
+//  Created by Zaid Neurothrone on 2022-12-13.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewTwoAndThreeColumnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
